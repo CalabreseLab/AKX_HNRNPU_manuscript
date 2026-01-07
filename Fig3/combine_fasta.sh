819e58aac870b831e749541cea4b3bf9861204cc
Shuang <57366173+Shuang-Plum@users.noreply.github.com>
2026-01-07 10:38:13 -0500

    Delete Fig3 directory

--- Fig3/combine_fasta.sh
+++
@@ -1,8 +0,0 @@
-
-
-ls Xist_c*_neg_merged100_200cut.fa | sort -V | xargs cat -- > Xist_all_200cutpeaks.fa
-
-ls Airn_c*_pos_merged100_200cut.fa | sort -V | xargs cat -- > Airn_all_200cutpeaks.fa
-
-ls Kot1_c*_neg_merged100_200cut.fa | sort -V | xargs cat -- > Kot1_all_200cutpeaks.fa
-
