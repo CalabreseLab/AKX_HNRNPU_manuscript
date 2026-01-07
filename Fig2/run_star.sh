6a0bd3a279d59d332a60e5ab7f439019df63dee3
Shuang <57366173+Shuang-Plum@users.noreply.github.com>
2026-01-07 10:37:48 -0500

    Delete Fig2 directory

--- Fig2/run_star.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p general
-#SBATCH -N 1
-#SBATCH -n 8
-#SBATCH --time=3:00:00
-#SBATCH --mem=64g
-
-
-STAR --genomeDir /rip/MM10_genome_index --runThreadN 8 --outMultimapperOrder Random --outSAMmultNmax 1 --readFilesIn /rip/rip_data/$1.fastq --outFileNamePrefix /rip/star_aligned/$1_
